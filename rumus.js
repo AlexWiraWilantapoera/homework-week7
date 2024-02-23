b10c76c744909f0c1afe55fdf25dc8a72ba05635
AlexWiraWilantapoera <alexwira99@gmail.com>
2024-02-23 16:40:42 +0700

    tambah fungsi rumus luas

--- rumus.js
+++ rumus.js
@@ -1,7 +1,15 @@
 exports.rumusKelilingPersegi = function (sisi) {
-  return `Keliling persegi dengan panjang sisi ${sisi}cm adalah ${4 * sisi}cm.`;
+  return `Keliling persegi dengan sisi ${sisi}cm adalah ${4 * sisi}cm.`;
+};
+
+exports.rumusLuasPersegi = function (sisi) {
+  return `Luas persegi dengan sisi ${sisi}cm adalah ${sisi * sisi}cm2.`;
 };
 
 exports.rumusKelilingPersegiPanjang = function (panjang, lebar) {
   return `Keliling persegi panjang dengan panjang ${panjang}cm dan lebar ${lebar}cm adalah ${2 * (panjang + lebar)}cm.`;
+};
+
+exports.rumusLuasPersegiPanjang = function (panjang, lebar) {
+  return `Luas persegi panjang dengan panjang ${panjang}cm dan lebar ${lebar}cm adalah ${panjang * lebar}cm2.`;
 };
--- soal1.js
+++ soal1.js
@@ -3,3 +3,7 @@ const rumus = require("./rumus.js");
 console.log(rumus.rumusKelilingPersegi(2));
 
 console.log(rumus.rumusKelilingPersegiPanjang(4, 8));
+
+console.log(rumus.rumusLuasPersegi(5));
+
+console.log(rumus.rumusLuasPersegiPanjang(5, 7));
